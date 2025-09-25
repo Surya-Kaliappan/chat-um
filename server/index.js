734cac54c6ab02f3a55ef238bded2ad768f1af6d
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-09-25 10:16:52 +0530

    Done upto profile management and authentication

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 1234;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin: process.env.ORIGIN, // get request from this link
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],  // REST Operations
+    credentials: true,  // to enable cookies
+}));
+
+app.use("/uploads/profiles", express.static("uploads/profiles"));
+
+app.use(cookieParser());  // getting cookies from frontend
+app.use(express.json());  // getting payload from the request
+
+app.use("/api/auth", authRoutes);
+
+const server = app.listen(port, () => {
+    console.log(`Server is running.. http://localhost:${port}`);
+});
+
+mongoose.connect(databaseURL)   // Connect to the Database
+.then(() => console.log("Database connected")).catch(err => console.log(err.message));
---
+++ server/models/UserModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { genSalt, hash } from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is Required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password i Required"],
+    },
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    color: {
+        type: Number,
+        required: false,
+    },
+    profileSetup: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+userSchema.pre("save", async function(next){  // Used to hash the password and store,
+    const salt = await genSalt();             // using bcrypt
+    this.password = await hash(this.password, salt);
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
