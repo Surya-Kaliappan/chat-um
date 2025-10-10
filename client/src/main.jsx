d2307b38d0d9f742f8af00c9d23e2eecf7630062
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-10-10 11:36:24 +0530

    setup github page deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,15 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner.jsx';
 import { SocketProvider } from './context/SocketContext';
-import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-
-  <HashRouter>
-    <SocketProvider>
-      <App />
-      <Toaster closeButton position="top-center" />
-    </SocketProvider>
-  </HashRouter>
-
+  // <StrictMode>
+  <SocketProvider>
+    <App />
+    <Toaster closeButton position="top-center" />
+  </SocketProvider>
+  // </StrictMode>,
 );
