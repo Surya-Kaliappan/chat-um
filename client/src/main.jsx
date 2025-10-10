4c0331be6759311051973db0778fa018ce08ae40
Surya-Kaliappan <kaliappansurya47@gmail.com>
2025-10-10 11:28:59 +0530

    setup github page deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { Toaster } from './components/ui/sonner.jsx';
 import { SocketProvider } from './context/SocketContext';
 import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
 
   <HashRouter>
     <SocketProvider>
